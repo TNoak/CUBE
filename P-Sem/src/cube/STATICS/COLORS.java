b329b39fd1ce395101a1064ba77ef67990d44ee6
TNoak <42678309+TNoak@users.noreply.github.com>
2018-09-30 15:59:43 +0200

    Add files via upload

---
+++ P-Sem/src/cube/STATICS/COLORS.java
@@ -0,0 +1,27 @@
+package cube.STATICS;
+
+import java.awt.Color;
+
+public class COLORS {
+
+	public static Color mainBackground = new Color(33,32,49);
+	public static Color lightBackground=new Color(54,53,80);
+	
+	public static Color greenDark=new Color(192,255,62);
+	
+	public static Color greyDark=new Color(130,152,156);
+	public static Color greenLight=new Color(26,226,157);
+	
+	public static Color buttonSensor=new Color(255,211,81);
+	
+	public static Color greyLight=new Color(200,200,206);
+	public static Color buttonRed=Color.RED;
+	public static Color buttonGreen= Color.GREEN;
+	public static Color white=new Color(255,255,255);
+	
+	public static Color temp=new Color(96,211,221);
+	public static Color temp2=new Color(74,166,174);
+	public static Color humid=new Color(5,214,165);
+	public static Color humid2=new Color(6,139,108);
+
+}
